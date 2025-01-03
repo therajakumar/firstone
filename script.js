dff527254ffd6d1d45488dd7aeacf151fb2c8892
therajakumar <rajak.ug21.me@nitp.ac.in>
2025-01-04 00:27:20 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+
+  searchForm.classList.remove("active");
+  cartItemsContainer.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+
+  cartItemsContainer.classList.remove("active");
+};
+
+let cartItemsContainer = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItemsContainer.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItemsContainer.classList.remove("active");
+};
